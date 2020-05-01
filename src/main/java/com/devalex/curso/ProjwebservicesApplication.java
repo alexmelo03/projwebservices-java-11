938b28f0fab77e089786120db8072e0c647dc402
alexmelo03 <alexmello79@yahoo.com.br>
2020-05-01 14:59:06 -0300

    Project created

---
+++ src/main/java/com/devalex/curso/ProjwebservicesApplication.java
@@ -0,0 +1,13 @@
+package com.devalex.curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjwebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjwebservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devalex/curso/ProjwebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devalex.curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjwebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
